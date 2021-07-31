def2693da215e8d6b50197655f3dc30cd258e005
Debian <admin@ip-172-31-57-179.ec2.internal>
2021-07-31 13:30:56 +0000

    completed debian

--- setup.sh
+++ setup.sh
@@ -88,10 +88,11 @@ function dockerin {
 	menu
 	elif [[ `cat /etc/os-release | grep "ID=debian"` ]]; then
 	echo -e "\n${GREEN}[${WHITE}+${GREENS}]${GREENS} Debian detected installing docker.........."
-	sudo apt remove docker docker-engine docker.io containerd runc && \
-	sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release && \
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
-	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+	sudo apt remove docker docker-engine docker.io && \
+	sudo apt-get update && \
+	sudo apt-get install apt-transport-https ca-certificates software-properties-common curl gnupg lsb-release && \
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" && \
 	sudo apt-get update && sudo apt install docker-ce docker-ce-cli containerd.io
 	else
 	echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager" && sleep 2 && menu;
